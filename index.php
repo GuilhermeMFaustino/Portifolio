21694f027a0a1f6187f039d2af0001d92c98c1fe
Guilherme Mendes <guilhermemendes.info@gmail.com>
2025-11-24 10:45:24 -0300

    desenvolvimento do portfólio pessoal

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+    <title>Guilherme Mendes</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <section class="logo">
+                <img src="imagens/logotipo.webp" alt="logotipo">
+            </section>
+
+            <nav class="menu-desktop">
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">sobre Mim</a></li>
+                    <li><a href="#">Habilidades</a></li>
+                    <li><a href="#">Portfólio</a></li>
+                    <li><a href="#"><button>Contato</button></a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <main>
+        <section class="hero">
+            <div class="container">
+                <div class="img-hero">
+                    <img src="imagens/hero-img.webp" alt="">
+                </div>
+
+                <div class="txt-hero">
+                    <h1>Ideias Digitais. <span>Resultados Reias</span></h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quaerat neque sunt atque id quidem, veniam qui tempora aspernatur repellat distinctio, explicabo consequuntur quod! Eum cupiditate dolores explicabo rem facilis!</p>
+                 <a href=""><button>Entre em Contato</button></a>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
